d92828c80366af0da5570bf9105483aee455149f
gareth <gareth@balancedlabs.com>
2015-10-23 23:50:58 +1100

    slight cleanup of backend

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -4,35 +4,13 @@ module Main where
 import Control.Monad (msum)
 import Happstack.Server
 
-import           Text.Blaze ((!))
-import qualified Text.Blaze.Html4.Strict as H
-import qualified Text.Blaze.Html4.Strict.Attributes as A
-
-appTemplate :: String -> [H.Html] -> H.Html -> H.Html
-appTemplate title headers body =
-    H.html $ do
-      H.head $ do
-        H.title (H.toHtml title)
-        H.meta ! A.httpEquiv "Content-Type"
-               ! A.content "text/html;charset=utf-8"
-        sequence_ headers
-      H.body $ do
-        body
-
 homepage :: ServerPart Response
-homepage =
-   ok $ toResponse $
-    appTemplate "Email Manager: (Trump Edition)"
-                [H.meta ! A.name "keywords"
-                        ! A.content "happstack, blaze, html"
-                ]
-                (H.p $ do "Hello, "
-                          H.b "blaze-html!")
+homepage = serveDirectory EnableBrowsing ["index.html"] "frontend/dist/"
 
 trumpApp :: ServerPart Response
 trumpApp = msum [
-              serveDirectory EnableBrowsing ["index.html"] "frontend/dist/"
-            ]
+              homepage
+           ]
 
 main :: IO ()
 main = do
