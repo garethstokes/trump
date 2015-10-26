146ab23a720992544041ee05d48498afa211d63b
gareth <gareth@balancedlabs.com>
2015-10-26 14:30:54 +1100

    backend: move web into a separate module

--- backend/src/Main.hs
+++ backend/src/Main.hs
@@ -2,18 +2,7 @@
 module Main where
 
 import Control.Monad (msum)
-import Happstack.Server
-
-homepage :: ServerPart Response
-homepage = serveDirectory EnableBrowsing ["index.html"] "frontend/dist/"
-
-trumpApp :: ServerPart Response
-trumpApp = msum [
-              -- TODO: api route goes here.
-              homepage
-           ]
+import qualified Web
 
 main :: IO ()
-main = do
-    let conf = nullConf { port = 1123 }
-    simpleHTTP conf $ trumpApp
+main = Web.app
