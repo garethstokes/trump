38fd72fe4df691a517719fbf656b3ade45d76378
gareth <gareth@balancedlabs.com>
2016-11-07 21:10:37 +1100

    sanity commit

--- backend/src/lib/Trump/DomainModel.hs
+++ backend/src/lib/Trump/DomainModel.hs
@@ -1,6 +1,9 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Trump.DomainModel where
 
+import Data.Aeson 
 import Data.DateTime
+import Data.ByteString.Lazy.Char8
 
 type EntityId = Int
 
@@ -22,3 +25,42 @@ data UserProfile = UserProfile {
                      userCreated   :: Maybe DateTime,
                      userModified  :: Maybe DateTime
                    } deriving (Show)
+
+-- |ToJSON instance allows us to encode a value as JSON.
+instance ToJSON UserProfile where
+  toJSON (UserProfile a b c d e f g) = object [ "id" .= a,
+                                                "name" .= b,
+                                                "email" .= c,
+                                                "refreshTokens" .= d,
+                                                "accessToken" .= e,
+                                                "dateCreated" .= f,
+                                                "dateModified" .= g ]
+
+instance ToJSON Token where
+  toJSON (Token a b c d e f) = object [ "id" .= a,
+                                        "key" .= b,
+                                        "value" .= c,
+                                        "isActive" .= d,
+                                        "dateCreated" .= e,
+                                        "dateModified" .= f ]
+
+instance FromJSON UserProfile where
+  parseJSON (Object v) =
+    UserProfile <$>
+    (v .: "userProfileId")    <*>
+    (v .: "name")             <*>
+    (v .: "email")            <*>
+    (v .: "refreshTokens")    <*>
+    (v .:? "accessToken")     <*>
+    (v .:? "userCreated")     <*>
+    (v .:? "userModified")
+
+instance FromJSON Token where
+  parseJSON (Object v) =
+    Token <$>
+    (v .: "tokenId")          <*>
+    (v .: "tokenKey")         <*>
+    (v .: "value")            <*>
+    (v .: "isActive")         <*>
+    (v .: "tokenCreated")     <*>
+    (v .: "tokenModified")
--- backend/src/lib/Trump/Web/Api.hs
+++ backend/src/lib/Trump/Web/Api.hs
@@ -1,20 +1,42 @@
 module Trump.Web.Api where
 
+import Control.Monad
 import Control.Monad.IO.Class
 import Happstack.Server
 import Trump.Data
 import Trump.DomainModel
+import Data.Aeson
 
 import qualified Trump.Database.Repository as R
+import qualified Data.ByteString.Lazy.Char8 as B
 
-apiOk :: String -> ServerPart Response
-apiOk r = 
-  ok $ (toResponse r) { rsHeaders=(mkHeaders [("Content-Type","application/json")]) }
+instance ToMessage UserProfile where
+    toContentType _ = toContentType "application/json"
+
+    toMessage d   = encode d
+    toMessage [d] = encode d
+
+{-
+ 
+  IO<UserProfile[]> userProfiles = R.getUserProfiles
+  
+  encode :: ToJSON a => a -> Data.ByteString.Lazy.Internal.ByteString
+  ByteString encode(UserProfile[]);
+
+  liftM :: Monad m => (a1 -> r) -> m a1 -> m r
+  IO<ByteString> liftM(Action<ByteString, UserProfile[]> a, IO<UserProfile[]> b);
+
+  IO<ByteString> result = liftM(encode, userProfiles)
+
+  ByteString str <- liftM encode $ R.getUserProfiles
+
+  >>= :: Monad m => m a -> (a -> m b) -> m b
+  
+-}
 
 getUsers :: ServerPart Response
-getUsers = do
-  users <- liftIO R.getUserProfiles
-  apiOk $ show users
+getUsers = ok R.getUserProfiles
+  --apiOk "userProfiles" $ liftM (B.unpack . encode) R.getUserProfiles
 
 createUser :: ServerPart Response
 createUser = ok (toResponse "create_user: success")
